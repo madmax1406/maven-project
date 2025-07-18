1fe5e6a272459030b6d5d9de3439e299296f705b
= <=>
2025-07-18 17:04:57 +0530

    updated java file for main class

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -5,6 +5,11 @@ package com.example;
  */
 public class Greeter {
 
+   public static void main(String[] args) {
+    Greeter greeter = new Greeter();
+    System.out.println(greeter.greet("World"));
+  }
+
   /**
    * This is a constructor.
    */
